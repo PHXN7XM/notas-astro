c351ee1819426ddea636d4ab34772d4c2397833c
PHXN7XM <07phxntxm@proton.me>
2026-01-14 21:09:15 -0600

    configuracion de ruta

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://phxn7xm.github.io',
+	site: 'https://PHXN7XM.github.io',
   	base: '/notas-astro',
 	integrations: [
 		starlight({
