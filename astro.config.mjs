378e5c7519b4587bdc3b5db1607f2c1e7ddbc18b
PHXN7XM <07phxntxm@proton.me>
2026-01-14 21:41:12 -0600

    fix link2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	site: 'https://PHXN7XM.github.io',
   	base: '/notas-astro',
+	trailingSlash: 'always',
 	integrations: [
 		starlight({
 			title: 'My Docs',
